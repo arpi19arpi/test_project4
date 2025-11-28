400328154cfe11c7ac7f1b7ff434e317a2d7ce75
arpi19arpi <arpi.mangasaryan.876@my.csun.edu>
2025-11-28 01:52:16 -0800

    Update

--- script.js
+++ script.js
@@ -5,40 +5,31 @@ async function getNASAData() {
   const response = await fetch(apiURL);
   const data = await response.json();
 
-  const img = document.getElementById("spaceImg");
-  const title = document.getElementById("title");
-  const desc = document.getElementById("desc");
-
-  title.textContent = data.title;
-  desc.textContent = data.explanation;
-
-  // Checking if NASA APOD is actually an image 
-  if (data.media_type === "image") {
-    img.src = data.url;
-
-    // --- SPRING ANIMATION ---
-    const { spring } = window.popmotion;
-
-    spring({
-      from: { scale: 0.5 },
-      to: { scale: 1 },
-      stiffness: 120,
-      damping: 10,
-      mass: 0.5,
-      onUpdate: latest => {
-        img.style.transform = `scale(${latest.scale})`;
-      }
-    }).start();
-
-  } else {
-    img.src = "https://via.placeholder.com/600x400?text=Today's+APOD+is+a+Video";
-  }
+  document.getElementById("spaceImg").src = data.url;
+  document.getElementById("title").textContent = data.title;
+  document.getElementById("desc").textContent = data.explanation;
 }
 
 getNASAData();
 
+// Using spring animation here on the button
+window.addEventListener("DOMContentLoaded", () => {
+  const { spring } = window.popmotion;
+  const btn = document.getElementById("openNasaBtn");
 
-// Button redirects 
+  spring({
+    from: { scale: 0.5 },
+    to: { scale: 1 },
+    stiffness: 150,
+    damping: 12,
+    mass: 0.5,
+    onUpdate: latest => {
+      btn.style.transform = `scale(${latest.scale})`;
+    }
+  }).start();
+});
+
+// Button to redirect to NASA website
 document.getElementById("openNasaBtn").addEventListener("click", () => {
   window.open("https://apod.nasa.gov/apod/astropix.html", "_blank");
 });
