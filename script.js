c5cc060462a2a4f75eabfb9141a569b25865160a
arpi19arpi <arpi.mangasaryan.876@my.csun.edu>
2025-11-28 02:27:27 -0800

    Refactor NASA button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Using NASA's API
 const apiURL = "https://api.nasa.gov/planetary/apod?api_key=8lX7oxoVj2NvzLhqOyZZmhotHUIqkmIzrzgdan0L";
 
-// Fetch NASA Image of the Day
 async function getNASAData() {
   const response = await fetch(apiURL);
   const data = await response.json();
@@ -11,26 +10,24 @@ async function getNASAData() {
   document.getElementById("desc").textContent = data.explanation;
 }
 
-// Calling the function 
+// load NASA data
 getNASAData();
 
-//Popmotion spring animation on the Button 
-const btn = document.getElementById("openNasaBtn");
-const { spring } = window.popmotion;
-
 // Using spring animation here on the button
-spring({
-  from: { scale: 0.5 },
-  to: { scale: 1 },
-  stiffness: 150,
-  damping: 12,
-  mass: 0.5,
-  onUpdate: latest => {
-    btn.style.transform = `scale(${latest.scale})`;
-  }
-}).start();
+const btn = document.getElementById("openNasaBtn");
 
-// Button Click will redirect to NASA's webpage
+// use the global popmotion object
+popmotion.spring({
+  from: 0.5,
+  to: 1,
+  stiffness: 120,
+  damping: 12
+}).start(v => {
+  // v is the animated value from 0.5 -> 1
+  btn.style.transform = `scale(${v})`;
+});
+
+// Button to redirect to NASA website
 btn.addEventListener("click", () => {
   window.open("https://apod.nasa.gov/apod/astropix.html", "_blank");
 });
